183c03971358dca0c60cb3b254131546bf0b8b18
reinaldo-santosBa <reinaldodevcontato@gmail.com>
2023-07-13 14:15:04 -0300

    feat: create method list aprovaded ServiceContractBulletinController

--- src/controllers/serviceContractBulletin.ts
+++ src/controllers/serviceContractBulletin.ts
@@ -2,6 +2,7 @@ import { Request, Response } from 'express'
 import { ServiceContractBulletinService } from '../services/serviceContractBulletin/listServiceContractBulletin'
 import { ListDetailsBulletin } from '../services/serviceContractBulletin/listDetailsService'
 import { ApprovalBulletinService } from '../services/serviceContractBulletin/approvalService'
+import { ServiceContractBulletinServiceApprovaded } from '../services/serviceContractBulletin/listServiceBulletinApprovaded'
 
 export class ServiceContractBulletinController {
   public async list (request: Request, response: Response): Promise<Response> {
@@ -14,6 +15,20 @@ export class ServiceContractBulletinController {
     const serviceContractBulletinService = new ServiceContractBulletinService()
 
     const result = await serviceContractBulletinService.execute(acessToken)
+
+    return response.json(result)
+  }
+
+  public async listAprrovaded (request: Request, response: Response): Promise<Response> {
+    const authHeader = request.headers.authorization
+    if (!authHeader) {
+      return response.status(400).json({ message: 'TOKEN IS MISSING' })
+    }
+    const [, acessToken] = authHeader.split(' ')
+
+    const serviceContractBulletinServiceApprovaded = new ServiceContractBulletinServiceApprovaded()
+
+    const result = await serviceContractBulletinServiceApprovaded.execute(acessToken)
 
     return response.json(result)
   }
