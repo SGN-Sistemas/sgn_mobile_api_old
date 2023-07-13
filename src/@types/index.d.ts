df65769d4926c640780574a682e59ab8be22c97b
reinaldo-santosBa <reinaldodevcontato@gmail.com>
2023-07-13 16:33:38 -0300

    feat: create a new param global user_sigla

--- src/@types/index.d.ts
+++ src/@types/index.d.ts
@@ -1,9 +1,10 @@
-import * as express from 'express'
+export {}
 declare global {
     namespace Express {
         export interface Request {
             user_cod: string;
             database: string;
+            user_sigla: string;
         }
     }
 }
