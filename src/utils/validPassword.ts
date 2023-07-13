c1208120f59d24d85767284a320a5e235a442881
reinaldo-santosBa <reinaldodevcontato@gmail.com>
2023-07-13 15:13:37 -0300

    refactor: add password verify length

--- src/utils/validPassword.ts
+++ src/utils/validPassword.ts
@@ -1,10 +1,25 @@
 import { passwordStrength } from 'check-password-strength'
 
 const validPassword = (password: string) => {
+  if (password.length < 10) {
+    return {
+      message: 'Senha de conter mais de 10 caracteres',
+      error: true,
+      status: 400
+    }
+  }
   if (passwordStrength(password).value === 'Strong') {
-    return true
+    return {
+      message: 'Senha deve ser forte',
+      error: true,
+      status: 400
+    }
   } else {
-    return false
+    return {
+      message: '',
+      error: false,
+      status: 200
+    }
   }
 }
 
