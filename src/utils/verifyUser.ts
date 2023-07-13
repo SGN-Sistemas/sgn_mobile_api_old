e3de4427898538df9f19a4f4b5abdbb024af7d92
reinaldo-santosBa <reinaldodevcontato@gmail.com>
2023-07-13 15:11:30 -0300

    refactor: reaname function verifyUser to verifyUserLoginLogin

--- src/utils/verifyUser.ts
+++ src/utils/verifyUser.ts
@@ -9,53 +9,54 @@ interface IVerifyUser {
     userCod: number | string;
 }
 
-export const verifyUser = async (acronym: string, password: string, database: string):Promise<IVerifyUser> => {
-  const sqlVerifyUser = verifyUserQuery(acronym, database)
-  const existsUser = await UsuarioRepository.query(sqlVerifyUser)
+export const verifyUserLogin =
+ async (acronym: string, password: string, database: string):Promise<IVerifyUser> => {
+   const sqlVerifyUser = verifyUserQuery(acronym, database)
+   const existsUser = await UsuarioRepository.query(sqlVerifyUser)
 
-  if (!existsUser[0]) {
-    return ({
-      message: 'Login ou senha incorreto',
-      error: true,
-      status: 400,
-      userCod: ''
-    })
-  }
+   if (!existsUser[0]) {
+     return ({
+       message: 'Login ou senha incorreto',
+       error: true,
+       status: 400,
+       userCod: ''
+     })
+   }
 
-  if (!existsUser[0].USUA_SENHA_APP || existsUser[0].USUA_SENHA_APP === '') {
-    return ({
-      message: 'Úsuario sem senha cadastrada',
-      error: true,
-      status: 400,
-      userCod: ''
-    })
-  }
+   if (!existsUser[0].USUA_SENHA_APP || existsUser[0].USUA_SENHA_APP === '') {
+     return ({
+       message: 'Úsuario sem senha cadastrada',
+       error: true,
+       status: 400,
+       userCod: ''
+     })
+   }
 
-  const passwordBD = existsUser[0].USUA_SENHA_APP
+   const passwordBD = existsUser[0].USUA_SENHA_APP
 
-  const comparePassword = await bcrypt.compare(password, passwordBD)
+   const comparePassword = await bcrypt.compare(password, passwordBD)
 
-  if (!comparePassword) {
-    return ({
-      message: 'Login ou senha incorreto',
-      error: true,
-      status: 400,
-      userCod: ''
-    })
-  }
+   if (!comparePassword) {
+     return ({
+       message: 'Login ou senha incorreto',
+       error: true,
+       status: 400,
+       userCod: ''
+     })
+   }
 
-  if (existsUser[0].USUA_BLOQ !== 'N') {
-    return ({
-      message: 'Úsuario bloqueado',
-      error: true,
-      status: 400,
-      userCod: ''
-    })
-  }
-  return ({
-    message: '',
-    error: false,
-    userCod: existsUser[0].USUA_COD,
-    status: 0
-  })
-}
+   if (existsUser[0].USUA_BLOQ !== 'N') {
+     return ({
+       message: 'Úsuario bloqueado',
+       error: true,
+       status: 400,
+       userCod: ''
+     })
+   }
+   return ({
+     message: '',
+     error: false,
+     userCod: existsUser[0].USUA_COD,
+     status: 0
+   })
+ }
